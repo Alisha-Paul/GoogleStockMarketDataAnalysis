cf8e13c26ff3291c1db2a30d562ddaa5d7400db9
Alisha Paul <35766667+Alisha-Paul@users.noreply.github.com>
2018-03-18 16:30:25 -0500

    Add files via upload

---
+++ GoogleStockMarketData.py
@@ -0,0 +1,8 @@
+import csv
+path = "google_stock_data.csv"
+file = open(path, newline='')
+reader = csv.reader(file)
+header = next(reader)
+data = [row for row in reader]
+print(header)
+print(data[0])
